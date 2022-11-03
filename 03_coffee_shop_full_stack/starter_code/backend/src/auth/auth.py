88b8345936174d95baa12932bc79b9bc56bcc072
hanicamunguambe <hanicamunguambe@gmail.com>
2022-11-03 21:23:34 +0200

    Fix bug: jose decote

--- 03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
+++ 03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
@@ -110,60 +110,77 @@ def check_permissions(permission, payload):
     !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
 '''
 def verify_decode_jwt(token):
-
     jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
     jwks = json.loads(jsonurl.read())
-    unverified_header = jwt.get_unverified_header(token)
-
+    unverified_header = jwt.get_unverified_header(token).get('kid')
+    rsa_key = {}
     if 'kid' not in unverified_header:
         raise AuthError({
             'code': 'invalid_header',
-            'description': 'Authorization malformated'
+            'description': 'Authorization malformed.'
         }, 401)
-    
-    key_rsa = {}
+
     for key in jwks['keys']:
         if key['kid'] == unverified_header['kid']:
-            key_rsa = {
+            rsa_key = {
                 'kty': key['kty'],
                 'kid': key['kid'],
                 'use': key['use'],
                 'n': key['n'],
                 'e': key['e']
             }
-    if key_rsa:
+    if rsa_key:
         try:
             payload = jwt.decode(
                 token,
-                key_rsa,
+                rsa_key,
                 algorithms=ALGORITHMS,
                 audience=API_AUDIENCE,
                 issuer='https://' + AUTH0_DOMAIN + '/'
-            ) 
+            )
+
             return payload
 
         except jwt.ExpiredSignatureError:
             raise AuthError({
                 'code': 'token_expired',
-                'description': 'Token expired'
-            }, 401)
-        
-        except jwt.JwtClaimsError:
-            raise AuthError({
-                'code': 'invalid_claims',
-                'description': 'Incorrect claims. Please check the issuer and audience'
+                'description': 'Token expired.'
             }, 401)
 
+        except jwt.JWTClaimsError:
+            raise AuthError({
+                'code': 'invalid_claims',
+                'description': 'Incorrect claims. Please, check the audience and issuer.'
+            }, 401)
         except Exception:
             raise AuthError({
                 'code': 'invalid_header',
-                'description': 'Unable to parse authentication token'
+                'description': 'Unable to parse authentication token.'
             }, 400)
-    
     raise AuthError({
-        'code': 'invalid_header',
-        'description': 'Unable to find the appropriate key'
-    }, 400)
+                'code': 'invalid_header',
+                'description': 'Unable to find the appropriate key.'
+            }, 400)
+# def get_unverified_header(token):
+
+#         try:
+#             get_token_auth_header()
+#         except (ValueError, TypeError):
+#             return {'error': 'access token error'}, 401
+
+#         return get_unverified_header(token)
+    
+
+# def jwt_refresh_token_needed(func):
+#     @wraps(func)
+#     def decorator(*args, **kwargs):
+#         try:
+#             verify_decode_jwt()
+#         except (ValueError, TypeError):
+#             return {'error': 'refresh token error'}, 401
+
+#         return func(*args, **kwargs)
+#     return decorator
 
 '''
 @TODO implement @requires_auth(permission) decorator method
@@ -180,8 +197,11 @@ def requires_auth(permission=''):
         @wraps(f)
         def wrapper(*args, **kwargs):
             token = get_token_auth_header()
-            payload = verify_decode_jwt(token)
-            check_permissions(permission, payload)
+            try:
+                payload = verify_decode_jwt(token)
+                check_permissions(permission, payload)
+            except:
+                abort(401) 
             return f(payload, *args, **kwargs)
 
         return wrapper
